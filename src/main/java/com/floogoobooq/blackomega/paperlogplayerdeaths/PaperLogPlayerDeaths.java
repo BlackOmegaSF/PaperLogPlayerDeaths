6a38a64c274328076e1aa5b73c3ff9a6c271f018
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-29 10:03:12 -0800

    Initial commit

---
+++ src/main/java/com/floogoobooq/blackomega/paperlogplayerdeaths/PaperLogPlayerDeaths.java
@@ -0,0 +1,10 @@
+package com.floogoobooq.blackomega.paperlogplayerdeaths;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaperLogPlayerDeaths extends JavaPlugin implements Listener {
+
+    
+
+}
